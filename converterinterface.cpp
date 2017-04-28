fc93921349abfa9233e2f0dac88bca26d5dd382c
hll cgl <halilcaglar26@hotmail.com>
2017-04-28 13:44:44 +0300

    converterInterface eklendi.

---
+++ converterinterface.cpp
@@ -0,0 +1,7 @@
+#include "converterinterface.h"
+
+converterInterface::converterInterface()
+{
+
+}
+
---
+++ converterinterface.h
@@ -0,0 +1,11 @@
+#ifndef CONVERTERINTERFACE_H
+#define CONVERTERINTERFACE_H
+
+
+class converterInterface
+{
+public:
+    converterInterface();
+};
+
+#endif // CONVERTERINTERFACE_H
